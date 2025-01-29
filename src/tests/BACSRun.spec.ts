91b0855bfe4798b2595d30cc4b440dfc17fe5757
pruthvirajqa2dev <pruthvirajqa2dev>
2025-01-29 12:37:06 +0530

    Update BACSRun spec with uncommented code

--- src/tests/BACSRun.spec.ts
+++ src/tests/BACSRun.spec.ts
@@ -73,188 +73,188 @@ test.describe("BACS Run " + `${process.env.test_env}`.toUpperCase(), () => {
             await prl614q.checkForEnteredConfirmedStatus();
         });
 
-        // screen = expectedTexts.PRL300Q;
+        screen = expectedTexts.PRL300Q;
 
-        // const prl300q: PRL300Q =
-        //     await test.step(`Go to the screen ${screen}`, async () => {
-        //         await homepage.clickHamburgerMenuButton();
-        //         await homepage.goToScreenUsingMenusOption(screen);
-        //         return new PRL300Q(page, testInfo);
-        //     });
-        // logger.info(`Navigate to screen ${screen}`);
+        const prl300q: PRL300Q =
+            await test.step(`Go to the screen ${screen}`, async () => {
+                await homepage.clickHamburgerMenuButton();
+                await homepage.goToScreenUsingMenusOption(screen);
+                return new PRL300Q(page, testInfo);
+            });
+        logger.info(`Navigate to screen ${screen}`);
 
-        // await test.step("Verify valid page elements are visible", async () => {
-        //     await prl300q.expectPageElementsVisibilityOnLoad();
-        // });
-        // await test.step("Select school Id and click on search", async () => {
-        //     await prl300q.selectSchoolId(expectedTexts.expectedSchoolName);
-        //     await prl300q.clickSearchBtn();
-        // });
+        await test.step("Verify valid page elements are visible", async () => {
+            await prl300q.expectPageElementsVisibilityOnLoad();
+        });
+        await test.step("Select school Id and click on search", async () => {
+            await prl300q.selectSchoolId(expectedTexts.expectedSchoolName);
+            await prl300q.clickSearchBtn();
+        });
 
-        // await test.step("Get the list of all the suppliers with payment method BACS and add invoices fot the same", async () => {
-        //     const fileName = await FileUtils.latestFileNameLookup(
-        //         `${process.cwd()}/Test Files/BACS*.TXT`
-        //     );
-        //     console.log(
-        //         "filename=" +
-        //             fileName.split("\\")[fileName.split("\\").length - 1]
-        //     );
-        //     const fileContent = await FileUtils.readFileAsync(fileName);
-        //     console.log("fileContent=" + fileContent);
+        await test.step("Get the list of all the suppliers with payment method BACS and add invoices fot the same", async () => {
+            const fileName = await FileUtils.latestFileNameLookup(
+                `${process.cwd()}/Test Files/BACS*.TXT`
+            );
+            console.log(
+                "filename=" +
+                    fileName.split("\\")[fileName.split("\\").length - 1]
+            );
+            const fileContent = await FileUtils.readFileAsync(fileName);
+            console.log("fileContent=" + fileContent);
 
-        //     const suppliersAcceptingBACS = fileContent.split(",");
-        //     expect(suppliersAcceptingBACS.length).toBeGreaterThan(0);
+            const suppliersAcceptingBACS = fileContent.split(",");
+            expect(suppliersAcceptingBACS.length).toBeGreaterThan(0);
 
-        //     logger.info(
-        //         `Suppliers accepting BACS payment method are: ${suppliersAcceptingBACS}`
-        //     );
-        //     const invoiceReferencesMap = new Map();
-        //     function addValueToKey(key, value) {
-        //         if (!invoiceReferencesMap.has(key)) {
-        //             invoiceReferencesMap.set(key, []);
-        //         }
-        //         invoiceReferencesMap.get(key).push(value);
-        //         logger.info(
-        //             `Key-value- ${key}:${invoiceReferencesMap.get(key)}`
-        //         );
-        //     }
-        // const countOfUniqueSuppliers = 3;
-        // const countOfInvoicesPerSupplier = 1;
-        //     for (let counter = 0; counter < countOfUniqueSuppliers ; counter++) {
-        //         for (let i = 0; i < countOfInvoicesPerSupplier; i++) {
-        //             let randomIndex;
-        //             const costCentre =
-        //                 InvoiceCalc.costCentreArr[
-        //                     Math.floor(
-        //                         Math.random() * InvoiceCalc.costCentreArr.length
-        //                     )
-        //                 ];
-        //             logger.info("Cost Centre: " + costCentre);
-        //             randomIndex = Math.floor(
-        //                 Math.random() * InvoiceCalc.quantArr.length
-        //             );
-        //             logger.info("Random Index quantity: " + randomIndex);
+            logger.info(
+                `Suppliers accepting BACS payment method are: ${suppliersAcceptingBACS}`
+            );
+            const invoiceReferencesMap = new Map();
+            function addValueToKey(key, value) {
+                if (!invoiceReferencesMap.has(key)) {
+                    invoiceReferencesMap.set(key, []);
+                }
+                invoiceReferencesMap.get(key).push(value);
+                logger.info(
+                    `Key-value- ${key}:${invoiceReferencesMap.get(key)}`
+                );
+            }
+            const countOfUniqueSuppliers = 3;
+            const countOfInvoicesPerSupplier = 1;
+            for (let counter = 0; counter < countOfUniqueSuppliers; counter++) {
+                for (let i = 0; i < countOfInvoicesPerSupplier; i++) {
+                    let randomIndex;
+                    const costCentre =
+                        InvoiceCalc.costCentreArr[
+                            Math.floor(
+                                Math.random() * InvoiceCalc.costCentreArr.length
+                            )
+                        ];
+                    logger.info("Cost Centre: " + costCentre);
+                    randomIndex = Math.floor(
+                        Math.random() * InvoiceCalc.quantArr.length
+                    );
+                    logger.info("Random Index quantity: " + randomIndex);
 
-        //             const quantity = InvoiceCalc.quantArr[randomIndex];
-        //             logger.info("Quantity: " + quantity);
-        //             randomIndex = Math.floor(
-        //                 Math.random() * InvoiceCalc.unitPriceArr.length
-        //             );
-        //             logger.info("Random Index unit price: " + randomIndex);
-        //             const unitPrice = InvoiceCalc.unitPriceArr[randomIndex];
-        //             logger.info("Unit Price: " + unitPrice);
+                    const quantity = InvoiceCalc.quantArr[randomIndex];
+                    logger.info("Quantity: " + quantity);
+                    randomIndex = Math.floor(
+                        Math.random() * InvoiceCalc.unitPriceArr.length
+                    );
+                    logger.info("Random Index unit price: " + randomIndex);
+                    const unitPrice = InvoiceCalc.unitPriceArr[randomIndex];
+                    logger.info("Unit Price: " + unitPrice);
 
-        //             randomIndex = Math.floor(
-        //                 Math.random() * InvoiceCalc.vatCodeArr.length
-        //             );
-        //             logger.info("Random Index vatcode: " + randomIndex);
-        //             const vatCode = InvoiceCalc.vatCodeArr[randomIndex];
+                    randomIndex = Math.floor(
+                        Math.random() * InvoiceCalc.vatCodeArr.length
+                    );
+                    logger.info("Random Index vatcode: " + randomIndex);
+                    const vatCode = InvoiceCalc.vatCodeArr[randomIndex];
 
-        //             logger.info("VAT Code: " + vatCode);
-        //             const vatPercent = InvoiceCalc.calcVatPercent(vatCode);
-        //             logger.info("VAT Percent: " + vatPercent);
-        //             let netInvoice =
-        //                 Math.round(quantity * unitPrice * 100) / 100;
-        //             logger.info("Net Invoice: " + netInvoice);
-        //             const vatAmount =
-        //                 Math.round(((netInvoice * vatPercent) / 100) * 100) /
-        //                 100;
-        //             logger.info("VAT Amount: " + vatAmount);
-        //             const expectedTotalInvoiceValue =
-        //                 Math.round((netInvoice + vatAmount) * 100) / 100;
-        //             logger.info(
-        //                 "Total Invoice Value: " + expectedTotalInvoiceValue
-        //             );
-        //             await prl300q.clickEsrMultiBtnUsingText(
-        //                 expectedTexts.newText
-        //             );
-        //             logger.info("Click new button with dropdown");
+                    logger.info("VAT Code: " + vatCode);
+                    const vatPercent = InvoiceCalc.calcVatPercent(vatCode);
+                    logger.info("VAT Percent: " + vatPercent);
+                    let netInvoice =
+                        Math.round(quantity * unitPrice * 100) / 100;
+                    logger.info("Net Invoice: " + netInvoice);
+                    const vatAmount =
+                        Math.round(((netInvoice * vatPercent) / 100) * 100) /
+                        100;
+                    logger.info("VAT Amount: " + vatAmount);
+                    const expectedTotalInvoiceValue =
+                        Math.round((netInvoice + vatAmount) * 100) / 100;
+                    logger.info(
+                        "Total Invoice Value: " + expectedTotalInvoiceValue
+                    );
+                    await prl300q.clickEsrMultiBtnUsingText(
+                        expectedTexts.newText
+                    );
+                    logger.info("Click new button with dropdown");
 
-        //             if (
-        //                 expectedTexts.invoiceType ==
-        //                 "Non Purchase Order Invoice"
-        //             ) {
-        //                 logger.info(
-        //                     "Checking non purchase order invoice radio button"
-        //                 );
-        //                 await prl300q.checkNonPurchaseOrderInvoiceRadioBtn();
-        //             }
-        //             await prl300q.click(prl300q.selectBtnLocator);
+                    if (
+                        expectedTexts.invoiceType ==
+                        "Non Purchase Order Invoice"
+                    ) {
+                        logger.info(
+                            "Checking non purchase order invoice radio button"
+                        );
+                        await prl300q.checkNonPurchaseOrderInvoiceRadioBtn();
+                    }
+                    await prl300q.click(prl300q.selectBtnLocator);
 
-        //             await prl300q.click(prl300q.supplierIconLocator);
-        //             logger.info(
-        //                 "Double clicking supplier " +
-        //                     suppliersAcceptingBACS[i] +
-        //                     " from lookup popup"
-        //             );
-        //             await prl300q.clickSelectForSupplierName(
-        //                 suppliersAcceptingBACS[i]
-        //             );
+                    await prl300q.click(prl300q.supplierIconLocator);
+                    logger.info(
+                        "Double clicking supplier " +
+                            suppliersAcceptingBACS[i] +
+                            " from lookup popup"
+                    );
+                    await prl300q.clickSelectForSupplierName(
+                        suppliersAcceptingBACS[i]
+                    );
 
-        //             logger.info(
-        //                 "Selecting supplier and verifying supplier text"
-        //             );
-        //             await prl300q.verifySupplierTextOnOrderDetailsScreen(
-        //                 suppliersAcceptingBACS[i]
-        //             );
-        //             // await page.waitForLoadState("load");
-        //             await prl300q.fillInvoiceDateOnOrderDetailsScreen();
+                    logger.info(
+                        "Selecting supplier and verifying supplier text"
+                    );
+                    await prl300q.verifySupplierTextOnOrderDetailsScreen(
+                        suppliersAcceptingBACS[i]
+                    );
+                    // await page.waitForLoadState("load");
+                    await prl300q.fillInvoiceDateOnOrderDetailsScreen();
 
-        //             await prl300q.fillInvoiceFiguresOnOrderDetailsScreen(
-        //                 expectedTotalInvoiceValue.toString(),
-        //                 vatAmount.toString()
-        //             );
-        //             logger.info("Filled invoice date and figures");
+                    await prl300q.fillInvoiceFiguresOnOrderDetailsScreen(
+                        expectedTotalInvoiceValue.toString(),
+                        vatAmount.toString()
+                    );
+                    logger.info("Filled invoice date and figures");
 
-        //             const invoiceReference: string =
-        //                 "INV" +
-        //                 Math.floor(Math.random() * 1000000) +
-        //                 counter.toString();
-        //             logger.info("Invoice Reference: " + invoiceReference);
-        //             await prl300q.fillInvoiceReferenceOnOrderDetailsScreen(
-        //                 invoiceReference
-        //             );
-        //             addValueToKey(
-        //                 "invoiceRef" + i.toString(),
-        //                 invoiceReference
-        //             );
+                    const invoiceReference: string =
+                        "INV" +
+                        Math.floor(Math.random() * 1000000) +
+                        counter.toString();
+                    logger.info("Invoice Reference: " + invoiceReference);
+                    await prl300q.fillInvoiceReferenceOnOrderDetailsScreen(
+                        invoiceReference
+                    );
+                    addValueToKey(
+                        "invoiceRef" + i.toString(),
+                        invoiceReference
+                    );
 
-        //             //***************Line Details screen******************
-        //             await prl300q.addLineDetails(costCentre);
-        //             await prl300q.fillQuantityAndUnitPrice(
-        //                 quantity.toString(),
-        //                 unitPrice.toString()
-        //             );
-        //             await prl300q.selectVatCode(vatCode);
-        //             const expectedVatAmount = String(
-        //                 vatAmount !== 0
-        //                     ? Intl.NumberFormat("en-US", {
-        //                           minimumFractionDigits: 2
-        //                       }).format(Number(vatAmount.toFixed(2)))
-        //                     : "0.00"
-        //             );
-        //             await prl300q.expectVatValueToBeCalculated(
-        //                 expectedVatAmount.toString()
-        //             );
-        //             await prl300q.expectTotalInvoiceValueToBeCalculated(
-        //                 expectedTotalInvoiceValue.toString()
-        //             );
-        //             await prl300q.click(prl300q.saveBtnLocator);
-        //             await prl300q.compareQuantityAndUnitPriceOnLineGrid(
-        //                 quantity,
-        //                 unitPrice
-        //             );
-        //             await prl300q.compareLineGridDetails(netInvoice, vatAmount);
-        //             await prl300q.compareVatCodeOnLineGrid(vatCode);
-        //             await prl300q.click(prl300q.finishAndSaveBtnLocator);
+                    //***************Line Details screen******************
+                    await prl300q.addLineDetails(costCentre);
+                    await prl300q.fillQuantityAndUnitPrice(
+                        quantity.toString(),
+                        unitPrice.toString()
+                    );
+                    await prl300q.selectVatCode(vatCode);
+                    const expectedVatAmount = String(
+                        vatAmount !== 0
+                            ? Intl.NumberFormat("en-US", {
+                                  minimumFractionDigits: 2
+                              }).format(Number(vatAmount.toFixed(2)))
+                            : "0.00"
+                    );
+                    await prl300q.expectVatValueToBeCalculated(
+                        expectedVatAmount.toString()
+                    );
+                    await prl300q.expectTotalInvoiceValueToBeCalculated(
+                        expectedTotalInvoiceValue.toString()
+                    );
+                    await prl300q.click(prl300q.saveBtnLocator);
+                    await prl300q.compareQuantityAndUnitPriceOnLineGrid(
+                        quantity,
+                        unitPrice
+                    );
+                    await prl300q.compareLineGridDetails(netInvoice, vatAmount);
+                    await prl300q.compareVatCodeOnLineGrid(vatCode);
+                    await prl300q.click(prl300q.finishAndSaveBtnLocator);
 
-        //             await prl300q.dismissErrorPrompt();
-        //             await prl300q.expectOrderDetailsScreenToBeVisible(
-        //                 vatAmount
-        //             );
-        //         }
-        //     }
-        // });
+                    await prl300q.dismissErrorPrompt();
+                    await prl300q.expectOrderDetailsScreenToBeVisible(
+                        vatAmount
+                    );
+                }
+            }
+        });
         var screen = expectedTexts.PRL614Q;
         var prl614q =
             await test.step(`Go to the screen ${screen}`, async () => {
