3d20f368c8da24b5e2841819fff4082b9eb61e45
pruthvirajqa2dev <154661815+pruthvirajqa2dev@users.noreply.github.com>
2026-06-18 10:27:00 +0100

    fix: new config for merge

---
+++ playwright.merge.config.ts
@@ -0,0 +1,15 @@
+// playwright.merge.config.ts
+
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+    reporter: [
+        [
+            "html",
+            {
+                outputFolder: "playwright-report",
+                open: "never"
+            }
+        ]
+    ]
+});
